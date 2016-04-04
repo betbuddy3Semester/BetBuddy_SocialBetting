aa8ec6e6023c81df4278759618fc14784570023e
Alex-Pedersen <1038281@ucn.dk>
2016-04-04 10:54:55 +0200

    Tilføjelse af forskellige referencer

--- BetBuddy/BetBuddy/Controllers/ManageController.cs
+++ BetBuddy/BetBuddy/Controllers/ManageController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
