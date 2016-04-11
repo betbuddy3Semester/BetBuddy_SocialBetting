95281e09e52ff6bd9ad5198327d8d91d6918c44b
Alex-Pedersen <1038281@ucn.dk>
2016-04-11 10:34:01 +0200

    Test of test commited

---
+++ BetBuddy/BetBuddy.Tests/UnitTest2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BetBuddy.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int i = 5;
+            int e = 5;
+
+            //Act
+            var x = i + e;
+
+            //Assure
+            Assert.AreEqual(10, x);
+        }
+    }
+}
