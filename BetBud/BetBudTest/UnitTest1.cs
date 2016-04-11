5ea40ba247565668c3a01161f2fa338ed26d9cb6
Alex-Pedersen <1038281@ucn.dk>
2016-04-11 20:49:24 +0200

    Creating folders, and the initial first test. Checking to see if deployment is correct, also changed the framework to 4.5

---
+++ BetBud/BetBudTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace BetBudTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            var i = 5;
+
+            //Act
+            var iRes = i + 2;
+
+            //Assert
+            Assert.AreEqual(iRes, 7);
+
+        }
+    }
+}
