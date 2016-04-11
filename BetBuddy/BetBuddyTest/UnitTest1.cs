b5e815d39bc84e8a375ef88c9f731d15efb48b14
Alex-Pedersen <1038281@ucn.dk>
2016-04-11 10:54:48 +0200

    Tests

---
+++ BetBuddy/BetBuddyTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace BetBuddyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var i = 5;
+
+            Assert.AreEqual(i, 5);
+        }
+    }
+}
